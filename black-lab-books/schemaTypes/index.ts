c0d4313f82a54f2eb9b7fca942cada1a7425f6e2
awimsatt1 <awimsatt99@gmail.com>
2026-03-09 00:17:18 -0500

    Remove duplicate schemaTypes folder

--- black-lab-books/schemaTypes/index.ts
+++
@@ -1,6 +0,0 @@
-import {postType} from './postType'
-import {product} from './product'
-import {publisher} from './publisher'
-import {event} from './event'
-
-export const schemaTypes = [postType, product, publisher, event]
